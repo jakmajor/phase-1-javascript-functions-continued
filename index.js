0a17fe07914cfb57140a93e48f4861b454115edd
Jak Major <hellojakkaiser@gmail.com>
2021-08-31 09:20:48 -0700

    commit tree 63f0580420980647aa5f67977447e79b4e5748c2

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity, activityDefault = "go to the office") {
+   
+    let content = activityDefault;
+    if (activity != undefined) {
+        content = activity;
+    }
+
+    return `This Monday, I will ${content}.`;
+}
+
+function wrapAdjective(flair = '*') {
+    return function inner(words = "special") {
+        return `You are ${flair}${words}${flair}!`
+    }
+}
+
+// console.log(wrapAdjective("!!!")("the best person ever"));
+
